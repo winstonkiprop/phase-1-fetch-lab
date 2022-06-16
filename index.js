d0ddbfaafb70b141535fce8cd069a6b1affe3c1c
winston kiprop <winstonekip50@gmail.com>
2022-06-16 11:14:04 +0300

    commit tree 8e841a28e0157c07aff905c3e87a33e94338a333

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(result => renderBooks(result));
 }
 
 function renderBooks(books) {
